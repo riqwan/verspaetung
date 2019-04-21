90abd9245ecebbca95dab4a170d7d58de1b9bee4
riqwan <rmthamir@gmail.com>
2019-04-21 17:46:20 +0200

    Added seed data to work with

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,66 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+def csv_rows_for(csv_file_name)
+  data_folder = Rails.root.join('db/data')
+  csv_text = File.read(data_folder.join(csv_file_name))
+
+  CSV.parse(csv_text, headers: true)
+end
+
+ActiveRecord::Base.transaction do
+  # Import lines.csv
+  TransportLine.destroy_all
+
+  csv_rows_for('lines.csv').each do |row|
+    TransportLine.create!(
+      id: (row['line_id'].to_i + 1),
+      name: row['line_name'],
+    )
+  end
+
+  # Import delays.csv
+  TransportDelay.destroy_all
+
+  csv_rows_for('delays.csv').each do |row|
+    TransportDelay.create!(
+      transport_line: ::TransportLine.find_by(name: row['line_name']),
+      delay: row['delay']
+    )
+  end
+
+  # Handling transport stop
+  TransportStop.destroy_all
+
+  csv_rows_for('stops.csv').each do |row|
+    TransportStop.create!(
+      id: (row['stop_id'].to_i + 1),
+      x: row['x'],
+      y: row['y'],
+    )
+  end
+
+  # Handling transport stop
+  TransportTime.destroy_all
+
+  csv_rows_for('times.csv').each do |row|
+    TransportTime.create!(
+      transport_line: TransportLine.find((row['line_id'].to_i + 1)),
+      transport_stop: TransportStop.find((row['stop_id'].to_i + 1)),
+      time: row['time'].to_datetime,
+    )
+  end
+
+  # Create schedule for next 30 days
+  # Every scheduled trip needs to be tracked. This is just my
+  # OCD in play. Not required for the task.
+  30.times do |day_index|
+    csv_rows_for('times.csv').each do |row|
+      TransportTime.create!(
+        transport_line: TransportLine.find((row['line_id'].to_i + 1)),
+        transport_stop: TransportStop.find((row['stop_id'].to_i + 1)),
+        time: (row['time'].to_datetime + (day_index + 1)),
+      )
+    end
+  end
+end
+
